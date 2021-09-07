62ddd8bdf763c5a4c7203fd9af4bc0de68d24d6a
【★༒✞︎Îτß-ℓєgєи∂-ℓx】 <87700009+LEGEND-LX@users.noreply.github.com>
2021-09-07 00:10:27 -0700

    Create jarvisfedban.py

---
+++ jarvisfedban.py
@@ -0,0 +1,104 @@
+
+import datetime
+import asyncio
+from telethon import events
+from telethon.errors.rpcerrorlist import YouBlockedUserError, UserAlreadyParticipantError
+from telethon.tl.functions.account import UpdateNotifySettingsRequest
+from telethon.tl.functions.messages import ImportChatInviteRequest
+from LEGENDBOT.utils import admin_cmd, edit_or_reply, sudo_cmd
+import time
+from userbot import ALIVE_NAME
+
+naam = str(ALIVE_NAME)
+
+bot = "@jarvisofficialsecuritybot"
+
+@bot.on(admin_cmd("fedban ?(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return    
+    sysarg = event.pattern_match.group(1)
+    if sysarg == "":
+      async with bot.conversation(bot) as conv:
+          try:
+              await conv.send_message("/start")
+              response = await conv.get_response()
+              await conv.send_message("/fban")
+              audio = await conv.get_response()
+              final = ("If you would like to know more about fban, use visit @jarvisofficialsecuritybot." , "")
+              await bot.send_message(event.chat_id, audio.text)
+              await event.delete()
+          except YouBlockedUserError:
+              await event.edit("**Error:** `unblock` @jarvisofficialsecuritybot `and retry!")
+    elif "@" in sysarg:
+      async with bot.conversation(bot) as conv:
+          try:
+              await conv.send_message("/start")
+              response = await conv.get_response()
+              await conv.send_message("/fban " + sysarg)
+              audio = await conv.get_response()
+              final = ("If you would like to know more about fban, please visit @jarvisofficialsecuritybot." , "")
+              await bot.send_message(event.chat_id, audio.text)
+              await event.delete()
+          except YouBlockedUserError:
+              await event.edit("**Error:** `unblock` @jarvisofficialsecuritybot `and try again!")
+    elif "" in sysarg:
+      async with bot.conversation(bot) as conv:
+          try:
+              await conv.send_message("/start")
+              response = await conv.get_response()
+              await conv.send_message("/fban " + sysarg)
+              audio = await conv.get_response()
+              final = ("If you would like to know more about fban, please visit @jarvisofficialsecuritybot." , "")
+              await bot.send_message(event.chat_id, audio.text)
+              await event.delete()
+          except YouBlockedUserError:
+              await event.edit("**Error:** `unblock` @jarvisofficialsecuritybot `and try again!")
+
+
+
+
+
+
+
+@bot.on(admin_cmd("unfedban ?(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return    
+    sysarg = event.pattern_match.group(1)
+    if sysarg == "":
+      async with bot.conversation(bot) as conv:
+          try:
+              await conv.send_message("/start")
+              response = await conv.get_response()
+              await conv.send_message("/unfban")
+              audio = await conv.get_response()
+              final = ("If you would like to know more about fban, use visit @jarvisofficialsecuritybot." , "")
+              await bot.send_message(event.chat_id, audio.text)
+              await event.delete()
+          except YouBlockedUserError:
+              await event.edit("**Error:** `unblock` @jarvisofficialsecuritybot `and retry!")
+    elif "@" in sysarg:
+      async with bot.conversation(bot) as conv:
+          try:
+              await conv.send_message("/start")
+              response = await conv.get_response()
+              await conv.send_message("/unfban " + sysarg)
+              audio = await conv.get_response()
+              final = ("If you would like to know more about fban, please visit @jarvisofficialsecuritybot." , "")
+              await bot.send_message(event.chat_id, audio.text)
+              await event.delete()
+          except YouBlockedUserError:
+              await event.edit("**Error:** `unblock` @jarvisofficialsecuritybot `and try again!")
+    elif "" in sysarg:
+      async with bot.conversation(bot) as conv:
+          try:
+              await conv.send_message("/start")
+              response = await conv.get_response()
+              await conv.send_message("/unfban " + sysarg)
+              audio = await conv.get_response()
+              final = ("If you would like to know more about fban, please visit @jarvisofficialsecuritybot." , "")
+              await bot.send_message(event.chat_id, audio.text)
+              await event.delete()
+          except YouBlockedUserError:
+              await event.edit("**Error:** `unblock` @jarvisofficialsecuritybot `and try again!")
