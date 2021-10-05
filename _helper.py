6587ebd1d2ff3568c73b3511cb7bf1e44d68ddb3
【★༒✞︎Îτß-ℓєɠεɳ∂-ℓх】 <87700009+LEGEND-LX@users.noreply.github.com>
2021-10-05 01:09:52 -0700

    Update _helper.py

--- _helper.py
+++ _helper.py
@@ -15,7 +15,7 @@ async def yardim(event):
     tgbotusername = Config.BOT_USERNAME
     input_str = event.pattern_match.group(1)
     if tgbotusername is not None or PYTHON_input == "text":
-        results = await event.client.inline_query(tgbotusername, "@PYTHON_Userbot")
+        results = await event.client.inline_query(tgbotusername, "@Python_Userbot_Support")
         await results[0].click(
             event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
         )
